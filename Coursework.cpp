6287abb7f94494f302095e6a003c7c7dd33e50fb
melihmehmedd <121875313+melihmehmedd@users.noreply.github.com>
2023-01-06 23:42:26 +0200

    Add files via upload

---
+++ Coursework.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Class Portable_Computers
+    {                
+Private:
+   String m_strProducer;
+   Int m_iScreenSize
+   Enum m_storageType
+   Int m_iCapacity
+Public  Explicit Computer_Producer()
+{
+   Cout << ”Vuvedete Proizvoditel = \n”;
+  Cin >> m_strProducer;
+  Cout << ”Vuvedete Razmer na Ekrana v inchove = \n“;
+  Cin >> m_iScreenSize;
+  Cout << ”Vuvedete Type na Diska SSD / HDD = \n“;
+  Cin >> m_storageType;
+  Cout << ”Vuvedete Golemina na Diska = \n”;
+  Cin >> m_iCapacity;
+}
+Public explicit Computer_Producer()
+{
+Cout << ”Vuvedete Proizvoditel = \n”;
+  Cin >> m_strProducer;
+GetProducer()
+
+}
+Public explicit Size_of_Screen()
+{
+Cout << ”Vuvedete Razmer na Ekrana v inchove = \n“;
+  Cin >> m_iScreenSize;
+GetScreenSize()
+Cout << ”Molia, vuvedete ime na vhodnia input fail : “;
+String Ime;
+Cin >> Ime;
+Ifstream Ime(ime.c_str(); 
+If(!ist) error(“Íå ìîæå äà ñå îòâîğè âõîäíèÿ ôàéë“, Ime);
+}
+Public Type_of_Disk()
+{
+Cout << ”Vuvedete Type na Diska SSD / HDD = \n“;
+  Cin >> m_storageType;
+GetDriveType()
+}
+Public Size_of_Disk()
+{
+Cout << ”Vuvedete Golemina na Diska = \n”;
+  Cin >> m_iCapacity;
+GetCapacity()
+}
+Void main() void
+{
+Computer_Producer();
+Size_of_Screen();
+Type_of_Disk();
+Size_of_Disk();
+return 0;
+}
+
+    }
